4b30ccdcc535498b23eb10314513274f4cdd8cf0
Anthony.Harris <anthony.harris@socreate.it>
2022-09-06 17:37:20 -0700

    changing title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-music-cheat-sheets';
+  title = 'my-music-cheat-sheets 2';
 
   filter: 'all' | 'active' | 'readyToPerform' = 'all';
 
